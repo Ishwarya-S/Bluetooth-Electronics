87619cf383df2afc527671d1b5a1576082a061ff
Ishwarya-S <iswarya@bluetronics.co.in>
2016-07-15 22:54:52 -0700

    Add files via upload

---
+++ Led increament and decreament using bluetooth/multiple_led.ino
@@ -0,0 +1,59 @@
+char junk;
+String inputString="";
+
+void setup()                    // run once, when the sketch starts
+{
+ Serial.begin(9600);            // set the baud rate to 9600, same should be of your Serial Monitor
+ pinMode(9, OUTPUT);
+ pinMode(10, OUTPUT);
+ pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  if(Serial.available()){
+  while(Serial.available())
+    {
+      char inChar = (char)Serial.read(); //read the input
+      inputString += inChar;        //make a string of the characters coming on serial
+     }
+    Serial.println(inputString);
+    while (Serial.available() > 0)  
+    { junk = Serial.read() ; }      // clear the serial buffer
+    if(inputString == "1"){         //in case of 'a' turn the LED on
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+    }
+    else if(inputString == "a"){   //incase of 'b' turn the LED off
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, LOW);
+    }
+      
+        
+    else if(inputString == "b"){   //incase of 'b' turn the LED off
+      digitalWrite(9, HIGH);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, HIGH);
+    }
+    else if(inputString == "c"){   //incase of 'b' turn the LED off
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(9, HIGH);
+    }
+    else if(inputString == "d"){   //incase of 'b' turn the LED off
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, HIGH);
+    }
+     else if(inputString == "e"){   //incase of 'b' turn the LED off
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(9, LOW);
+    }
+    
+  
+    inputString = "";
+  }
+}
---
+++ ultrabuzzer/ultrabuzzer.ino
@@ -0,0 +1,72 @@
+#define trigPin 11
+#define echoPin 12
+#define led1 8
+#define led2 7
+#define led3 10
+#define buzzer 3
+#define MAX_DISTANCE 200
+
+
+int sound = 250;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+ 
+}
+
+void loop() {
+  unsigned int duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delay(50);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+
+
+  Serial.println("cm");
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+ 
+
+  if (distance <= 25) {
+    digitalWrite(led1, HIGH);
+    sound = 250;
+}
+  else {
+    digitalWrite(led1,LOW);
+  }
+  if (distance < 15) {
+      digitalWrite(led2, HIGH);
+      sound = 260;
+}
+  else {
+      digitalWrite(led2, LOW);
+  }
+  if (distance < 10) {
+      digitalWrite(led3, HIGH);
+      sound = 270;
+} 
+  else {
+    digitalWrite(led3, LOW);
+  }
+ 
+  if (distance > 30 || distance <= 0){
+    Serial.println("Out of range");
+    noTone(buzzer);
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+    tone(buzzer, sound);
+   
+  }
+  delay(500);
+}
